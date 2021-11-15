fe99f004062eacbd33207f12cc3b2b123a2ed5e5
Alan Christian <alanchristiancreator@hotmail.com>
2021-11-15 20:53:36 -0300

    __INITIAL_COMMIT_CONFIG__

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c58e21a21ce80cd3dc9f81427e5219db'),
+    },
+  },
+})
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1952025d-7e31-437d-b969-765889a61c3c',
+}
